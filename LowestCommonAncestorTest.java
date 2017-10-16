5b7e730216a2da5f208055f8a9b9ad3eda6cdf32
osullr <32296368+osullr@users.noreply.github.com>
2017-10-16 12:50:35 +0100

    Added three tests

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -5,8 +5,24 @@ import org.junit.Test;
 public class LowestCommonAncestorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmptyTree() {
+		LowestCommonAncestor emptyTree = new LowestCommonAncestor();
+		assertEquals("The lowest common ancestor of an empty tree", -1, emptyTree.LowestCommonAncestor(0,0));
+	}
+	
+	@Test
+	public void testOneNode(){
+		LowestCommonAncestor oneNode = new LowestCommonAncestor();
+		oneNode.root = new Node(1);
+		assertEquals("The lowest common ancestor of a tree with one node", -1, oneNode.LowestCommonAncestor(1,0));
+	}
+	
+	@Test
+	public void testTwoNodes(){
+		LowestCommonAncestor twoNodes = new L=LowestCommonAncestor();
+		twoNodes.root = new Node(1);
+		twoNodes.root = new Node(2);
+		assertEquals("The lowest common ancestor of a tree with two nodes", 1, twoNodes.LowestCommonAncestor(1,2));
 	}
 
 }
