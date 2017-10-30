05b93864b2dbc345f5824054ec593ec11a988084
osullr <32296368+osullr@users.noreply.github.com>
2017-10-30 12:09:18 +0000

    DAG class
    Added methods from binary tree class

--- DAG.java
+++ DAG.java
@@ -1,20 +1,78 @@
 import java.util.ArrayList;
+import java.util.List;
 
-class Node<T> {
+class Node {
 	int data;
-	ArrayList<Node<T>> childNodes;
-	int child[];
+	ArrayList<Node> parentNodes;
+	Node left;
+	Node right;
 
 	Node(int value) {
-		this.data = value;
-		childNodes = new ArrayList<Node<T>>();
-		child = null;
+		data = value;
+		parentNodes = new ArrayList<Node>();
+		left = right;
+		left = null;
+		right = null;
 	}
 }
 
 public class DAG {
 
 	Node root;
+	private List<Integer> path1 = new ArrayList<>();
+	private List<Integer> path2 = new ArrayList<>();
+
+	int LowestCommonAncestor(int node1, int node2) {
+		path1.clear();
+		path2.clear();
+		return findLowestCommonAncestorBinaryTree(root, node1, node2);
+
+	}
+
+	private int findLowestCommonAncestorBinaryTree(Node root, int node1, int node2) {
+		if (!findPathBinaryTree(root, node1, path1) || !findPathBinaryTree(root, node2, path2)) {
+			if (path2.size() == 0 && path2.size() == 0) {
+				System.out.println("The tree is empty");
+			}
+			if (path1.size() > 0) {
+				System.out.println("Node 1 is present");
+			}
+			if (path2.size() > 0) {
+				System.out.println("Node 2 is present");
+			}
+			return -1;
+		}
+		int i;
+		for (i = 0; i < path1.size() && i < path2.size(); i++) {
+			if (!path1.get(i).equals(path2.get(i)))
+				break;
+		}
+		return path1.get(i - 1);
+
+	}
+
+	private boolean findPathBinaryTree(Node root, int n, List<Integer> path) {
+		if (root == null) {
+			return false;
+		}
+		path.add(root.data);
+
+		if (root.data == n) {
+			return true;
+		}
+
+		if (root.left != null && findPathBinaryTree(root.left, n, path)) {
+			return true;
+		}
+
+		if (root.right != null && findPathBinaryTree(root.right, n, path)) {
+			return true;
+		}
+
+		path.remove(path.size() - 1);
+
+		return false;
+	}
 
 	public Node getLCA(ArrayList<Node> graph, Node node1, Node node2) {
 
@@ -58,8 +116,8 @@ public class DAG {
 
 	private boolean isGraphAcyclic(ArrayList<Node> graph, ArrayList<Node> listToCompare, ArrayList<Node> checked,
 			boolean notAcyclic, Node index) {
-		for (int i = 0; i < index.childNodes.size(); i++) {
-			Node currentNode = (Node) index.childNodes.get(i);
+		for (int i = 0; i < index.parentNodes.size(); i++) {
+			Node currentNode = (Node) index.parentNodes.get(i);
 			if (!checked.contains(currentNode))
 				isGraphAcyclic(graph, listToCompare, checked, notAcyclic, index);
 			else if (listToCompare.contains(currentNode)) {
