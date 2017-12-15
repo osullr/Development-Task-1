242e91133d91fda01ffaa206529c70c59a90300a
osullr <32296368+osullr@users.noreply.github.com>
2017-12-15 14:13:58 +0000

    Add files via upload
    Fixed warnings that were coming up

--- CS3012 - Github Access.R
+++ CS3012 - Github Access.R
@@ -2,6 +2,7 @@ library(jsonlite)
 library(httpuv)
 library(httr)
 
+
 oauth_endpoints("github")
 
 myapp <- oauth_app(appname = "Software Engineering",
@@ -11,10 +12,6 @@ myapp <- oauth_app(appname = "Software Engineering",
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 gtoken <- config(token = github_token)
-
-github_url = "https://api.github.com"
-users_url = "https://api.github.com/users"
-repository_url = "https://api.github.com/users/repos"  
 
 req <- GET("https://api.github.com/users/osullr", gtoken)
 
@@ -28,41 +25,52 @@ gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 #Source:https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
 
 
-followersData = GET("https://api.github.com/users/osullr/followers", gtoken)
-followersID = followersData$login
-followersIDs = c(followersID)
-totalFollowers = c()
+
+followingData = GET("https://api.github.com/users/defunkt/following", gtoken )
+followingDataContent = content(followingData)
+followingDataDF = jsonlite::fromJSON(jsonlite::toJSON(followingDataContent))
+
+followingID = followingDataDF$login
+userIDs = c(followingID)
+allUsers = c()
+usersDF = data.frame(Username = integer(), Following = integer(), Followers = integer(), Repositories = integer())
 
 
-
-for(i in 1:length(totalFollowers)){
-  url = paste("https://api.github.com/users", followersIDs[i], "/followers", sep = "")
-  followers = fromJSON(url)
-  followersLogin = followers$login
-  for(j in 1:length(followersLogin)){
-    if(is.element(followersLogin[j], totalFollowers) == FALSE){
-      totalFollowers[[length(totalFollowers)+1]]=followersLogin[j]
-    }
+for(i in 1:length(userIDs)){
+  following_url = paste("https://api.github.com/users/", userIDs[i], "/following", sep = "")
+  following = GET(following_url, gtoken)
+  followingContent = content(following)
+  
+  if(length(followingContent)==0){
     next
   }
-  next
-}
-
-
-followingData = GET("https://api.github.com/users/osullr/following", gtoken)
-followingID = followingData$login
-followingIDs = c(followingID)
-totalFollowing = c()
-
-for(i in 1:length(totalFollowing)){
-  url = paste("https://api.github.com/users", followingIDs[i], "/following", sep = "")
-  following = fromJSON(url)
-  followingLogin = following$login
+  followingDF = jsonlite::fromJSON(jsonlite::toJSON(followingContent))
+  followingLogin = followingDF$login
+  
+  
+  
   for(j in 1:length(followingLogin)){
-    if(is.element(followingLogin[j], totalFollowing) == FALSE){
-      totalFollowing[[length(totalFollowing)+1]]=followingLogin[j]
+    if(is.element(followingLogin[j], allUsers) == FALSE){
+      allUsers[length(allUsers)+1]=followingLogin[j]
+      
+      followers_url = paste("https://api.github.com/users/", followingLogin[j], sep = "")
+      followers = GET(followers_url, gtoken)
+      followersContent = content(followers)
+      followersDF = jsonlite::fromJSON(jsonlite::toJSON(followersContent))
+      
+      numberFollowing = followersDF$following
+      numberFollowers = followersDF$followers
+      
+      numberRepos = followersDF$public_repos
+      
+      usersDF[nrow(usersDF)+ 1, ] = c(followingLogin[j], numberFollowing, numberFollowers, numberRepos)
+    }
+    
+    if(length(allUsers)>200){
+      break
     }
     next
   }
   next
 }
+
